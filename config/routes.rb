bda2c586b176ce6ee04e9e970eab55eb232c8726
Biondo Anthony <anthony.biondo@kedgebs.com>
2024-03-27 15:59:42 +0100

    un peu beaucoup passionnement pas du tout de front

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   end
   # resources :transport, only: [:update]
 
-  resources :compensation_trip, only: [:create]
+  resources :compensation_trips, only: [:create, :show, :index]
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
