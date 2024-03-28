35366eff5394f85a961fa113af4b596df9301b99
florian wagrez <florian.wagrez@gmail.com>
2024-03-28 17:22:15 +0100

    Compensations and stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,5 +8,14 @@ class PagesController < ApplicationController
     @trips = current_user.trips.reject { |trip| trip.transport.nil? }
     @compensations = Compensation.all
 
+    @carbon_sum = 0
+    @compensation_sum = 0
+
+    @trips.each do |trip|
+      @carbon_sum += trip.transport.co2_capacity
+      trip.compensation_trips.each do |compensation_trip|
+        @compensation_sum += compensation_trip.compensation.co2_absorption
+      end
+    end
   end
 end
