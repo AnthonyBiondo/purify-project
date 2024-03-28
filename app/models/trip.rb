bb0b89dc159c01f350c049a87427d7baf57a2c4d
florian wagrez <florian.wagrez@gmail.com>
2024-03-28 09:44:25 +0100

    Du back sur les compensations

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,8 +3,12 @@ class Trip < ApplicationRecord
   has_one :transport, dependent: :destroy
   has_one :transportTypes, through: :transport
 
-  has_many :compensationTrips
-  has_many :compensations, through: :compensationTrip
+  has_many :compensation_trips
+  has_many :compensations, through: :compensation_trips
     # geocoded_by :destination
     # after_validation :geocode, if: :will_save_change_to_destination?
+
+    def name
+      "#{destination} / #{departure}"
+    end
 end
---
+++ test/controllers/compensations_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class CompensationsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
