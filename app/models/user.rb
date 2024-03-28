e335fbc15c4c3e5b99ee82b5185469c69e01da1d
Biondo Anthony <anthony.biondo@kedgebs.com>
2024-03-28 16:05:01 +0100

    add page error

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :trips
+  has_many :trips, dependent: :destroy
   has_one :transport
 end
